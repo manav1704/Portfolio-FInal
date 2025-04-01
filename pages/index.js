9d839d4bb9a7c946e5aa8d5367a362ea3d159eea
manav1704 <manavsrivastava4@gmail.com>
2025-04-01 12:54:49 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,23 @@
 import Head from 'next/head';
-import { Hero, Work } from '../components';
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import Work from '../components/Work';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
-    <>
+    <div className="app">
       <Head>
-        <title>My Portfolio</title>
+        <title>Your Name | Portfolio</title>
+        <meta name="description" content="Developer portfolio" />
       </Head>
-      <Hero />
-      <Work />
-    </>
+
+      <Navbar />
+      <main>
+        <Hero />
+        <Work />
+      </main>
+      <Footer />
+    </div>
   );
 }
